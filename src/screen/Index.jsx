c477051dfae7da0f131491661048dfa824c5c70f
adyatmaa <adyatmaa001@gmail.com>
2026-04-21 16:43:40 +0700

    add shadcn

---
+++ src/screen/Index.jsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+export const Index = () => {
+  return (
+    <section>
+        <p>this is index</p>
+        <Button>Button</Button>
+    </section>
+  )
+}
